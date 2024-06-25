c7d9d9d2ac202fc1a688ce5bf872647144cdabaf
woowooyong <woowooyong@gmail.com>
2024-06-25 16:29:53 +0800

    feat: install unplugin-auto-import

---
+++ src/components/global/AppHeader.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+<template>
+  <header class="flex justify-between">
+    <div>This is Header</div>
+    <nav>
+      <ul class="flex items-center gap-3">
+        <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/about">About</RouterLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped></style>
