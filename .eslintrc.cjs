36026e00f1101cf3713d6c38bd6a0df88b44f49e
woowooyong <woowooyong@gmail.com>
2024-06-25 15:16:07 +0800

    style: update eslint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,16 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  extends: [
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off'
   }
 }
