d8d0e2d199517923dfd23208fa0ab1ce2defc706
woowooyong <woowooyong@gmail.com>
2024-06-25 16:06:40 +0800

    feat: install tailwind css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="mt-10 text-center text-2xl font-bold text-teal-700">Hello This Is My Template</h1>
   </main>
 </template>
