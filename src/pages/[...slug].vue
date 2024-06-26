f5eeab000e73322ca2773d3dcb46c82400e36dcd
woowooyong <woowooyong@gmail.com>
2024-06-26 14:20:11 +0800

    feat: add 404 Not Found page

---
+++ src/pages/[...slug].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="font-bol mt-10 text-center text-2xl font-bold text-stone-500">404 NOT FOUND</h1>
+  </div>
+</template>
+
+<style scoped></style>
